d3caf62bd9ce1e030e44630cf5342c00dd6327c3
Samoon <sadehghan@ce.sharif.edu>
2020-07-24 00:33:29 -0400

    Tab bar navigation style has been done. (fixes #7)

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import IconWithBadge from './components/IconWithBadge';
 
 function FeedScreen({ navigation }) {
   return (
@@ -40,12 +43,37 @@ const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Mail" component={MailScreen} />
-      <Tab.Screen name="Learn" component={LearnScreen} />
-      <Tab.Screen name="Share" component={ShareScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'lightslategrey',
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={{ tabBarIcon: ({ focused, color, size }) => (<IconWithBadge name={focused ? "ios-paper" : 'md-paper'} color={color} size={size} badgeCount={0} />) }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{ tabBarIcon: ({ focused, color, size }) => (<IconWithBadge name={focused ? "ios-chatbubbles" : 'md-chatbubbles'} color={color} size={size} badgeCount={1} />) }}
+      />
+      <Tab.Screen
+        name="Mail"
+        component={MailScreen}
+        options={{ tabBarIcon: ({ focused, color, size }) => (<IconWithBadge name={focused ? "ios-mail-open" : 'ios-mail'} color={color} size={size} badgeCount={0} />) }}
+      />
+      <Tab.Screen
+        name="Learn"
+        component={LearnScreen}
+        options={{ tabBarIcon: ({ focused, color, size }) => (<IconWithBadge name={focused ? "ios-book" : 'md-book'} color={color} size={size} badgeCount={0} />) }}
+      />
+      <Tab.Screen
+        name="Share"
+        component={ShareScreen}
+        options={{ tabBarIcon: ({ focused, color, size }) => (<IconWithBadge name={focused ? "ios-cloudy" : 'md-cloud-outline'} color={color} size={size} badgeCount={0} />) }}
+      />
     </Tab.Navigator>
   );
 }
@@ -56,7 +84,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeTabs} />
+        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeTabs} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ components/IconWithBadge.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const IconWithBadge = props => {
+    return (
+        <View style={styles.icon}>
+            <Ionicons name={props.name} size={props.size} color={props.color} />
+            {props.badgeCount > 0 && (
+                <View style={styles.badgeIcon}>
+                    <Text style={styles.badge}>
+                        {props.badgeCount}
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
+};
+
+styles = StyleSheet.create({
+    icon: {
+        width: 24,
+        height: 24,
+        margin: 5
+    },
+    badgeIcon: {
+        position: 'absolute',
+        right: -6,
+        top: -3,
+        backgroundColor: 'red',
+        borderRadius: 6,
+        width: 12,
+        height: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    badge: {
+        color: 'white',
+        fontSize: 10,
+        fontWeight: 'bold'
+    },
+});
+
+export default IconWithBadge;
